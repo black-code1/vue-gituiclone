2168db7be7abe71d96c04d1a59374b2f286b4b2f
black-code1 <legrandmahamat1@gmail.com>
2022-06-01 13:14:06 +0100

    Done Top Navs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.vue"],
+
+  theme: {
+    extend: {
+      spacing: {
+        72: "18rem",
+      },
+    },
+  },
+  plugins: [],
+};
